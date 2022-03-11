857f465cacc3b2ef15319d0edf20d493a9398142
ivanasitumorang <ivana.natalia@mhealth.tech>
2022-03-11 21:20:35 +0700

    feat(dashboard): add page dashboard ui

--- app/src/main/java/com/project/bankingapp/feature/DashboardActivity.kt
+++ app/src/main/java/com/project/bankingapp/feature/DashboardActivity.kt
@@ -1,12 +1,29 @@
 package com.project.bankingapp.feature
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.project.bankingapp.R
+import androidx.appcompat.app.AppCompatActivity
+import com.project.bankingapp.common.showToast
+import com.project.bankingapp.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupUIListener()
+    }
+
+    private fun setupUIListener() = with(binding) {
+        tvLogout.setOnClickListener {
+            showToast("logout")
+        }
+
+        btnTransfer.setOnClickListener {
+            showToast("navigate to page transfer")
+        }
     }
 }
