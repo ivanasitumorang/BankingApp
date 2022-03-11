bbdeaa6bf71fb5997aa006992b01f1511c984636
ivanasitumorang <ivana.natalia@mhealth.tech>
2022-03-12 00:12:07 +0700

    feat(transaction): add dummy data

--- app/src/main/java/com/project/bankingapp/feature/dashboard/Transaction.kt
+++ app/src/main/java/com/project/bankingapp/feature/dashboard/Transaction.kt
@@ -13,3 +13,63 @@ data class Transaction(
 enum class TransactionType {
     Income, Expense
 }
+
+data class TransactionHistory(
+    val dateString: String,
+    val transactions: List<Transaction>
+)
+
+val transactions1 = listOf(
+    Transaction(
+        id = "1",
+        amount = 1_000.0,
+        date = DateTime("2022-03-11T11:47:04.706Z"),
+        type = TransactionType.Expense,
+        recipientName = Recipient(accountNo = "8768-232-8233", accountHolder = "Dia")
+    ),
+    Transaction(
+        id = "",
+        amount = 2.0,
+        date = DateTime("2022-03-11T12:47:04.706Z"),
+        type = TransactionType.Income,
+        recipientName = Recipient(accountNo = "8999-324-9632", accountHolder = "Siapa")
+    ),
+    Transaction(
+        id = "",
+        amount = 30.0,
+        date = DateTime("2022-03-11T08:47:04.706Z"),
+        type = TransactionType.Income,
+        recipientName = Recipient(accountNo = "6554-630-9653", accountHolder = "Andy")
+    )
+)
+
+val transactions2 = listOf(
+    Transaction(
+        id = "1",
+        amount = 20_000.0,
+        date = DateTime("2022-05-11T11:47:04.706Z"),
+        type = TransactionType.Expense,
+        recipientName = Recipient(accountNo = "8768-232-8233", accountHolder = "Dia")
+    ),
+    Transaction(
+        id = "",
+        amount = 50.0,
+        date = DateTime("2022-05-11T12:47:04.706Z"),
+        type = TransactionType.Expense,
+        recipientName = Recipient(accountNo = "8999-324-9632", accountHolder = "Siapa")
+    )
+)
+
+val trxHistory1 = listOf(
+    TransactionHistory(
+        dateString = DateTime("2022-03-11T12:47:04.706Z").toString("DD MMM YYYY"),
+        transactions = transactions1
+    )
+)
+
+val trxHistory2 = listOf(
+    TransactionHistory(
+        dateString = DateTime("2022-03-11T12:47:04.706Z").toString("DD MMM YYYY"),
+        transactions = transactions2
+    )
+)
